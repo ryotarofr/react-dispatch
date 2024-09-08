d86787eddf6f7d769391803174fb69fd3cfb6e48
ryotarofr <ryoryo.fr0608@gmail.com>
2024-09-08 17:26:04 +0900

    インデント修正

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <CounterProvider>
-      <CounterComponent />
-    </CounterProvider>
+          <CounterComponent />
+      </CounterProvider>
     </>
   )
 }
